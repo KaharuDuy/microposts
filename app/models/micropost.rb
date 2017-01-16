a8618ec842422a12edf643204370d24ee38f9274
KaharuDuy <kaharu.wp@gmail.com>
2017-01-16 15:52:59 +0000

    kadai7

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,10 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence:true
   validates :content, presence:true, length: { maximum: 140 }
+<<<<<<< HEAD
 
   has_many :microposts
+=======
+>>>>>>> origin/master
   
 end
