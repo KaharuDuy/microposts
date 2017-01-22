64200f5020c0be0e3d14c972891b78dceb824e9b
KaharuDuy <kaharu.wp@gmail.com>
2017-01-22 14:44:12 +0000

    kadai

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,21 @@ class FavoritesController < ApplicationController
     before_action :logged_in_user
     
     def create
-      @favorite = Favorite.create(user_id: current_user.id, micropost_id: params[:micropost_id])
+      @micropost = Micropost.find(params[:micropost_id]) 
+      current_user.favorite(@micropost)
+      respond_to do |format|
+        format.html { redirect_to @micropost }
+        format.js
+      end
     end
 
     def destroy
-      favorite = Favorite.find_by(user_id: current_user.id, micropost_id: params[:micropost_id])
-      favorite.destroy
+      @micropost = current_user.favorites.find(params[:id]).micropost
+      current_user.unfavorite(@micropost)
+      respond_to do |format|
+        format.html { redirect_to @micropost }
+        format.js
+      end
     end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,16 +34,16 @@ class User < ActiveRecord::Base
       end
       
       def favorite(micropost)
-          favorites.find_or_create_by(micropost_id: user.id)
+          favorites.find_or_create_by(micropost_id: micropost.id)
       end
       
-      def unfavorite(micropos)
+      def unfavorite(micropost)
           favorite = favorites.find_by(micropost_id: micropost.id)
           favorite.destroy if favorite
       end
       
       def favorite?(micropost)
-          favorites.include?(micropost)
+          favorite_microposts.include?(micropost)
       end
       
       def feed_items
